c5981469bbf93d288dcb76c6f595b1c8f7370628
David Whitt <davidwhitt00@gmail.com>
2018-08-06 15:28:53 -0400

    update for Nav, Emp, Job

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MaterialModule} from './material.module';
+import { MaterialModule } from './material.module';
+import { JobpostsComponent } from './jobposts/jobposts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    JobpostsComponent
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/jobposts/jobposts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jobposts',
+  templateUrl: './jobposts.component.html',
+  styleUrls: ['./jobposts.component.scss']
+})
+export class JobpostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
