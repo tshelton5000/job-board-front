3382db239049cd9610d94d6e4921c273f4c9a952
Tyler Shelton <tshelton5000@gmail.com>
2018-08-06 16:53:50 -0400

    this should be a working student module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,43 +4,25 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import {MaterialModule} from './material.module';
-import {SharedModule} from './shared/shared.module';
-=======
-import { MaterialModule } from './material.module';
-import { JobpostsComponent } from './jobposts/jobposts.component';
->>>>>>> c5981469bbf93d288dcb76c6f595b1c8f7370628
-=======
 import { MaterialModule } from './material.module';
 import { JobpostsComponent } from './jobposts/jobposts.component';
 import {SharedModule} from './shared/shared.module';
+import {StudentModule} from './student/student.module';
 
->>>>>>> d16ad9b5332c9d1a6a3c71c80cc1093475fe2a84
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    JobpostsComponent
-   
+    JobpostsComponent   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    SharedModule
-=======
-    
->>>>>>> c5981469bbf93d288dcb76c6f595b1c8f7370628
-=======
-    SharedModule
-
->>>>>>> d16ad9b5332c9d1a6a3c71c80cc1093475fe2a84
+    SharedModule,
+    StudentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
