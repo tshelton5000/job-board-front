0d1c037f5f25e1f32e3dbc8421583189c190e26d
Lawrence Milton <milton.lawrence44@gmail.com>
2018-08-09 14:57:32 -0400

    profile

--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {PagenotfoundComponent} from '../pagenotfound/pagenotfound.component';
-
+import {ProfileComponent} from '../profile/profile.component';
 
 const sharedRoutes: Routes = [
+    {path:'profile', component: ProfileComponent},
     {path:'**', component: PagenotfoundComponent},
 ];
 
--- src/app/student/student-routing.module.ts
+++ src/app/student/student-routing.module.ts
@@ -1,8 +1,13 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {ProfileComponent} from '../profile/profile.component';
+import { PagenotfoundComponent } from '../pagenotfound/pagenotfound.component';
 
 
-const studentRoutes: Routes = [];
+const studentRoutes: Routes = [
+    {path:'profile', component: ProfileComponent},
+    {path:'**', component: PagenotfoundComponent},
+];
 
 @NgModule({
     imports: [RouterModule.forChild(studentRoutes)],
