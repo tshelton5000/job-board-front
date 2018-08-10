6c0e59d8d40a3f0f0a810193f0cbb6d222b496d8
David Whitt <davidwhitt00@gmail.com>
2018-08-10 15:04:02 -0400

    emp update

--- src/app/employer/employer.module.ts
+++ src/app/employer/employer.module.ts
@@ -1,9 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+<<<<<<< HEAD
  @NgModule({
   imports: [
     CommonModule
   ],
   declarations: []
+=======
+import { MaterialModule } from '../material.module';
+import { EmployerRoutingModule} from './employer-routing.module';
+import { EmpDashboardComponent } from './emp-dashboard/emp-dashboard.component';
+import { JobsUpdateComponent } from './jobs-update/jobs-update.component';
+import { JobsCreateComponent } from './emp-dashboard/jobs-create/jobs-create.component';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module'
+
+@NgModule({
+  imports: [
+    FormsModule,
+    CommonModule,
+    MaterialModule,
+    EmployerRoutingModule,
+    SharedModule
+
+  ],
+  declarations: [EmpDashboardComponent, JobsUpdateComponent, JobsCreateComponent]
+>>>>>>> emp update
 })
 export class EmployerModule { }
---
+++ src/app/shared/job-details/job-details.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog,} from '../../../../node_modules/@angular/material';
+
+
+export interface DialogData {
+  details: string;
+}
+
+@Component({
+  selector: 'app-job-details',
+  templateUrl: './job-details.component.html',
+  styleUrls: ['./job-details.component.css']
+})
+export class JobDetailsComponent{
+  details: string;
+
+  constructor(public dialog: MatDialog) { }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(JobDetailsComponent, {
+      width: '250px',
+      data: {details: this.details}
+    });
+  
+
+  dialogRef.afterClosed().subscribe(result => {
+    console.log('The dialog was closed');
+    this.details = result;
+  });
+  }
+  ngOnInit() {
+}
+}
+
