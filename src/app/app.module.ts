f6add0f14cf6661de9262af2fd649e63ab00829d
Lawrence Milton <milton.lawrence44@gmail.com>
2018-08-13 10:27:21 -0400

    with-the-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,7 @@ import { MaterialModule } from './material.module';
 import { JobpostsComponent } from './jobposts/jobposts.component';
 import {SharedModule} from './shared/shared.module';
 import {StudentModule} from './student/student.module';
-
-
-=======
-import { EmployerModule } from './employer/employer.module'
+import { EmployerModule } from './employer/employer.module';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
