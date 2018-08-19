e176efbdb1b3e537ebcdfc5664999340dc64e756
Brandon Coflin <bcoflin16@gmail.com>
2018-08-19 12:54:00 -0400

    alljobsdone

--- src/app/jobposts/jobposts.component.ts
+++ src/app/jobposts/jobposts.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
-
+import { DetailsComponent } from '../employer/emp-dashboard/details/details.component';
+import {MatDialog} from '@angular/material';
 @Component({
   selector: 'app-jobposts',
   templateUrl: './jobposts.component.html',
   styleUrls: ['./jobposts.component.scss']
 })
 export class JobpostsComponent implements OnInit {
+jobs: any;
 
-
- constructor(private data: DataService){ }
+ constructor(private data: DataService, public dialog: MatDialog){ }
 
   ngOnInit() {
-     
+     this.data.getJobs().subscribe((res:any)=>console.log(this.jobs = res.jobs))
+    }
+    openDetails(){
+      this.dialog.open(DetailsComponent);
+      
     }
   }
 
